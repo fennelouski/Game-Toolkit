9d950d9a1887916e15d2e6981864ce07651423ed
fennelouski <nathanfennel@gmail.com>
2015-02-02 22:35:22 -0500

    Initial Commit

---
+++ Game Toolkit/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  Game Toolkit
+//
+//  Created by Developer Nathan on 2/2/15.
+//  Copyright (c) 2015 Nathan Fennel. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
